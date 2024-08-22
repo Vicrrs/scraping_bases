176cb7d3d7351dca89b5c7ab5b3ae3511a74a566
Vicrrs <enricofermi2019@gmail.com>
2024-08-22 13:45:44 -0400

    🚧: Adicionando acesso ao link de cada duvida

--- src/scraper.py
+++ src/scraper.py
@@ -1,5 +1,7 @@
 import httpx
+from bs4 import BeautifulSoup
 
+# Passo 1: Capturar o HTML da página principal
 url = 'https://centraldeatendimento.totvs.com/hc/pt-br/sections/1500000596481-Linha-Winthor/'
 
 headers = {
@@ -10,12 +12,32 @@ with httpx.Client(headers=headers) as client:
     # Primeiro request para obter cookies
     client.get('https://centraldeatendimento.totvs.com/')
     
-    # Segundo request com cookies
+    # Segundo request com cookies para obter o conteúdo da página principal
     response = client.get(url)
     html_content = response.text
 
+# Salvar o HTML completo em um arquivo para inspeção
 output_file = '../data/files/whinthor_documentation.html'
 with open(output_file, 'w', encoding='utf-8') as file:
     file.write(html_content)
 
 print(f'HTML content saved to {output_file}')
+
+# Passo 2: Parsear o HTML para encontrar o link do "0 - Framework"
+soup = BeautifulSoup(html_content, 'html.parser')
+framework_link = soup.find('a', {'title': '0 - Framework'})['href']
+
+# Construir a URL completa para o link do "0 - Framework"
+framework_url = f'https://centraldeatendimento.totvs.com{framework_link}'
+
+# Passo 3: Acessar o link do "0 - Framework" e capturar o HTML
+with httpx.Client(headers=headers) as client:
+    response = client.get(framework_url)
+    framework_html_content = response.text
+
+# Salvar o HTML do "0 - Framework" em um arquivo
+framework_output_file = '../data/files/framework_documentation.html'
+with open(framework_output_file, 'w', encoding='utf-8') as file:
+    file.write(framework_html_content)
+
+print(f'Framework HTML content saved to {framework_output_file}')
